aa37e76e89bb88487502a497f3c0acc6e5458ef2
Harshil Khatri <khatriharshil26@gmail.com>
2025-03-08 19:38:06 +0530

    Implement Chat model (OpenAI) in Langchain.

---
+++ langchain_framework/2.ChatModels/2_chatmodel_anthropic.py
@@ -0,0 +1,9 @@
+from langchain_anthropic import ChatAnthropic
+from decouple import config
+
+model = ChatAnthropic(model="claude-3-5-sonnet-20241022", api_key=config("ANTHROPIC_API_KEY"), temperature=0.5, max_completion_tokens=10)
+
+response = model.invoke("What is the capital of India ?")
+
+print(response)
+print(response.content)
