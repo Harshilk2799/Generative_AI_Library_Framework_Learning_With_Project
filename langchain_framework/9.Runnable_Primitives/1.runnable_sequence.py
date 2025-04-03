51270debfef1fa51a2a7e1fcb1b10285334100c8
Harshil Khatri <khatriharshil26@gmail.com>
2025-04-03 23:09:55 +0530

    Implementation of Runnable Sequence in Langchain.

---
+++ langchain_framework/9.Runnable_Primitives/1.runnable_sequence.py
@@ -0,0 +1,24 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from decouple import config
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence
+
+chat_model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"))
+
+prompt1 = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=["topic"]
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template="Explain the following joke - {text}",
+    input_variables=["text"]
+)
+
+chain = RunnableSequence(prompt1, chat_model, parser, prompt2, chat_model, parser)
+
+response = chain.invoke({"topic": "AI"})
+print(response)
