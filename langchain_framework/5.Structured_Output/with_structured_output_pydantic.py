0a8e740d675d2cf14dba5d82c193e8640d0ff0e5
Harshil Khatri <khatriharshil26@gmail.com>
2025-06-29 10:41:40 +0530

    Implementation of Structure output with pydantic in langchain

--- langchain_framework/5.Structured_Output/with_structured_output_pydantic.py
+++ langchain_framework/5.Structured_Output/with_structured_output_pydantic.py
@@ -1,10 +1,9 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
 from typing import Literal, Optional, List
 from pydantic import BaseModel, Field
+from langchain_groq import ChatGroq
 
-
-chat_model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"))
+chat_model = ChatGroq(model="deepseek-r1-distill-llama-70b", api_key=config("GROQ_API_KEY"))
 
 # Schema
 class Review(BaseModel):
