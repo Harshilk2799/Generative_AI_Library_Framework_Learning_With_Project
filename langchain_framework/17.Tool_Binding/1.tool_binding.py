a9dc29e1b62a227bad662a105e00cf65a33980d9
Harshil Khatri <khatriharshil26@gmail.com>
2025-08-10 19:36:29 +0530

    Implementation of Tool Bind in langchain.

---
+++ langchain_framework/17.Tool_Binding/1.tool_binding.py
@@ -0,0 +1,25 @@
+from langchain_core.tools import tool
+
+@tool
+def multiply(a: int, b: int)-> int:
+    """Given 2 numbers a and b this tool returns their product."""
+    return a * b
+
+result = multiply.invoke({"a": 10, "b": 12})
+print(result)
+
+# print(multiply.name)
+# print(multiply.description)
+# print(multiply.args)
+# print(multiply.args_schema.model_json_schema())
+
+# Note: Not every LLM has the capability to bind tools. Only a few LLMs have this capability.
+
+# Tool Binding
+from langchain_google_genai import ChatGoogleGenerativeAI
+from decouple import config
+
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"), temperature=0)
+
+llm_with_tools = model.bind_tools([multiply])
+print(llm_with_tools)
