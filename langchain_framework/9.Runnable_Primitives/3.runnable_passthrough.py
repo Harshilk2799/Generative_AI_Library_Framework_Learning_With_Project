68d19c1739f810cbc5e25f566f9a298191171ec1
Harshil Khatri <khatriharshil26@gmail.com>
2026-02-06 21:04:26 +0530

    change import in runnable in langchain.

--- langchain_framework/9.Runnable_Primitives/3.runnable_passthrough.py
+++ langchain_framework/9.Runnable_Primitives/3.runnable_passthrough.py
@@ -2,7 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema.runnable import RunnableParallel, RunnableSequence, RunnablePassthrough
+from langchain_core.runnables import RunnableParallel, RunnableSequence, RunnablePassthrough
 
 # Simple Example
 # passthrough = RunnablePassthrough()
