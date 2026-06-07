4312622af2afdc2b86d12745cb1b98055a71f30b
Harshil Khatri <khatriharshil26@gmail.com>
2026-06-07 10:28:51 +0530

    Implementation of Vector store retriever in langchain.

--- langchain_framework/13.Retrievers/2.vector_store_retriever.py
+++ langchain_framework/13.Retrievers/2.vector_store_retriever.py
@@ -1,7 +1,8 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
 from decouple import config
+from langchain_milvus import Milvus
+from uuid import uuid4
 
 documents = [
     Document(page_content="Langchain helps developers build LLM application easily."),
@@ -10,14 +11,24 @@ documents = [
     Document(page_content="OpenAI providees powerful embedding models.")
 ]
 
-embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=config("GOOGLE_GEMINI_API_KEY"))
+embedding_model = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", google_api_key=config("GOOGLE_GEMINI_API_KEY"))
 
-vectorstore = Chroma.from_documents(
-    documents = documents,
-    embedding = embedding_model,
-    collection_name = "my_collection"
+vectorstore= Milvus(
+    embedding_function=embedding_model,
+    connection_args={
+        "uri": config("MILVUS_ENDPOINT"), "token": config("MILVUS_TOKEN"), "db_name": "milvusdb"
+    },
+    index_params={
+        "index_type": "FLAT", "metric_type": "L2"
+    },
+    consistency_level="Strong",
+    drop_old=False,
+    collection_name="milvusCollection4",
+    # auto_id=True
 )
 
+uuids = [str(uuid4()) for _ in range(len(documents))]
+vectorstore.add_documents(documents=documents, ids=uuids)
 # Convert vectorstore into a retriever
 retriever = vectorstore.as_retriever(search_kwargs={'k': 2})
 
