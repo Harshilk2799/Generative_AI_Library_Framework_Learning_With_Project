d934ba2855215ad34b64bafd8c678fe6bbc81d9f
Harshil Khatri <khatriharshil26@gmail.com>
2026-06-08 09:51:17 +0530

    Implementation of tool execution with example in langchain.

--- langchain_framework/19.Tool_Execution/2.tool_execution.py
+++ langchain_framework/19.Tool_Execution/2.tool_execution.py
@@ -20,7 +20,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
 from langchain_core.messages import HumanMessage
 
-model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"), temperature=0)
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"), temperature=0)
 
 llm_with_tools = model.bind_tools([multiply])
 # print(llm_with_tools)
