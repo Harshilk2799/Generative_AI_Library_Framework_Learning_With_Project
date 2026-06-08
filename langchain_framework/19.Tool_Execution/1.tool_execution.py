386132119c217a71cd4f76d1509ceddd63d8da86
Harshil Khatri <khatriharshil26@gmail.com>
2026-06-08 09:51:02 +0530

    Implementation of tool execution in langchain

--- langchain_framework/19.Tool_Execution/1.tool_execution.py
+++ langchain_framework/19.Tool_Execution/1.tool_execution.py
@@ -20,7 +20,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
 from langchain_core.messages import HumanMessage
 
-model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"), temperature=0)
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=config("GOOGLE_GEMINI_API_KEY"), temperature=0)
 
 llm_with_tools = model.bind_tools([multiply])
 # print(llm_with_tools)
@@ -36,5 +36,6 @@ result = llm_with_tools.invoke("Can you multiply 3 with 10.")
 # print(final_result)
 
 
-final_result = multiply.invoke(result.tool_calls[0])
+# final_result = multiply.invoke(result.tool_calls[0])
+final_result = multiply.invoke(result.tool_calls[0]["args"])
 print(final_result)
